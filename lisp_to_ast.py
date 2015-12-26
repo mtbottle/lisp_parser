4e1338b121b977e4d59f1ec8067a2d12ca465aeb
Michelle Tran <email@michtran.ca>
2015-12-26 16:46:47 +0000

    Get to a state where it runs and passes the 4 test cases I've written

--- lisp_to_ast.py
+++ lisp_to_ast.py
@@ -10,41 +10,42 @@ def parse_lisp_root(lisp_code):
     result, last_bracket = parse_lisp_helper(lisp_code)
     return result
 
-def parse_lisp_helper(lisp_code):
-    """Helper method to parse the internals of the lisp code
-    return ast, index offset of the next )
+
+def parse_lisp_helper(lisp_code, start_index):
+    """ Helper method to parse the internals of the lisp code. 
+        param lisp_code: The code we want to parse
+        param start_index: The index of the opening bracket that we want to
+                           parse.
+        return ast: the abstract syntax tree
+        return end_index: the index of the final parenthesis
     """
-    lisp_code = lisp_code.strip()
-    opening_bracket = lisp_code.find("(", 1)
+    assert lisp_code[start_index] == "("
+    first_closing_paren = lisp_code.find(")", start_index)
+    next_opening_paren = lisp_code.find("(", start_index+1)
 
-    if lisp_code.startswith("("):
-        lisp_code = lisp_code[1:]
-    print lisp_code
-
-    # base case
-    if opening_bracket < 0:
-        closing_bracket = lisp_code.find(")")
-        print closing_bracket
-        item = split(lisp_code[:closing_bracket], " ")
-        if len(item) == 0:
-            return None, closing_bracket+1
-        if len(item) == 1:
-            return item[0], closing_bracket
-        return item, closing_bracket+1
+    # base case, no more opening paren
+    if next_opening_paren < 0 or next_opening_paren > first_closing_paren:
+        return split(lisp_code[start_index+1:first_closing_paren], " "), first_closing_paren
 
     # inductive case
-    ast = split(lisp_code[:opening_bracket], " ")
-    closing_bracket = lisp_code[:opening_bracket].find(")")
-    lisp_code = lisp_code[opening_bracket:]
-    while opening_bracket > 0 or lisp_code:
-        sub_ast, closing_bracket = parse_lisp_helper(lisp_code)
-        if sub_ast:
-            ast.append(sub_ast)
-        lisp_code = lisp_code[closing_bracket:]
-        opening_bracket = lisp_code.find("(")
+    ast = split(lisp_code[start_index+1: next_opening_paren], " ")
+    while next_opening_paren > 0 and lisp_code[first_closing_paren:]:
+        sub_ast, first_closing_paren = parse_lisp_helper(lisp_code,
+                                                         next_opening_paren)
+        next_opening_paren = lisp_code.find("(", first_closing_paren+1)
+        ast.append(sub_ast)
 
-    return ast, closing_bracket
+    # now finish parsing everything else after the last set of parens
+    if first_closing_paren > -1:
+        next_closing_paren = lisp_code.find(")", first_closing_paren+1)
+        ast.extend(split(lisp_code[first_closing_paren+1:next_closing_paren], " "))
+    else:
+        next_closing_paren = len(lisp_code)
 
+    return ast, next_closing_paren
 
 if __name__ == "__main__":
-    print parse_lisp_helper("(first (list 1 (+ 2 3) 9))")
+    print parse_lisp_helper("(first (list 1 (+ 2 3) 9))", 0)
+    print parse_lisp_helper("(first (list (+ 1 2) (- 4 1) 9 (+ 5 6)))", 0)
+    print parse_lisp_helper("(list 8 9 3)", 0)
+    print parse_lisp_helper("(cdr (list (car (list 1 (+ 2 5))) (- 2 4) 4 (- 5 8)))", 0)
